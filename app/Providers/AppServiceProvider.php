01a15ff41ec9afc2f422c5138cf6d4fe181380f0
Emerson Souza <5545064+emersonti@users.noreply.github.com>
2026-02-09 01:01:10 +0000

    Corrigir URL::forceHttps para usar a configuração de URL do ambiente

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceHttps();
+        URL::forceHttps(config('pp.url'));
 
         HasMany::macro('createUpdateOrDelete', function (iterable $records) {
             /** @var HasMany $hasMany */
